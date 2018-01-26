e719f72604960c6462e0fcf7339dbe9ce901c1dc
Sunhare <bgyoume@gmail.com>
2018-01-26 14:08:59 -0800

    3D Geometry Foundation
    Implementing 3D geometric functions to help in the knot detection functions

--- Project3_ECS129.py
+++ Project3_ECS129.py
@@ -1,53 +1,133 @@
-import random 
-#Takes in the endpoints of a line segment in euclidean space 
-#And outputs the Plucker Coordinates which I have to do more research on
-#Plucker Coordinates are nice apparently because of the Side-Operator property
-#Used to define line segments not centered at the origin
-#Which will allow you to tell if two lines are parallel, and if not they intersect
-#Parallel if side_operator(a,b) #where a and b are Plucker coordinates# returns 0
+import math
 
-#p = tip of line, q = base of line, but I have to confirm that, in my tests cases
-#I do the opposite, cause *shrug*
-def Plucker_Coordinates(px, py, pz, qx, qy, qz): #Or grassman coordinates? idk
-	a0 = px*qy - qx*py;
-	a1 = px*qz - qx*pz;
-	a2 = px - qx;
-	a3 = py*qz - qy*pz;
-	a4 = pz-qz;
-	a5 = qy - py;
+class Point: 
+	#Constructor, initialized to the zero vector if no input given
+	def __init__(self, x=0, y=0, z=0):
+		self.x = x #x_coordinate
+		self.y = y #y_coordinate
+		self.z = z #z_coordinate
 
-	#This algorithm returns the 2x2 minors of the following matrix:
-	#px py pz 1
-	#qx qy qz 1
-	#Returns 6 member array for side-operator function
+	#Used for when you're printing the object
+	def __str__(self):
+		return ( "(" + str(self.x) + ", " + str(self.y) + ", " + str(self.z) + ")" )
 
-	return [a0, a1, a2, a3, a4, a5];
-
-
-#Takes in two plucker coordinates and returns 0 if they're parallel
-#Similar to the dot product, but for lines not centered at the origin
-def Side_Operator(a, b):
-	side_operator_value = (a[0]*b[4]) + (a[1]*b[5]) + (a[2]*b[3]) + (a[3]*b[2]) + (a[4]*b[0]) + (a[5]*b[1]);
-	return side_operator_value;
+	#Overloading addition and subtraction to handle points
+	def __add__(self, rhs):
+		return ((self.x+rhs.x), (self.y + rhs.y), (self.z + rhs.z))
+	def __sub__(self, rhs):
+		return ((self.x-rhs.x), (self.y - rhs.y), (self.z - rhs.z))
 
 
 
+class Vector:
+	#Constructor, initialized to the zero vector if no input given
+	#A = starting point, B = ending point
+	def __init__(self, A = Point(), B = Point()):
+		(self.x, self.y, self.z) = B-A #Does B(x,y,z) – A(x,y,z)
+
+	#The euclidean length is alculated when called
+	@property
+	def length(self):
+		length = math.sqrt( (self.x**2 + self.y**2 + self.z**2))
+		return length
+
+	def __str__(self):
+		return ("<" + str(self.x) + ", " +str(self.y)+", "+str(self.z)+">")
+
+	#Dot Product of Self dot Right Hand Side
+	def dot(self, rhs):
+		return (self.x * rhs.x) + (self.y * rhs.y) + (self.z * rhs.z)
+
+	#Cross Product of Self cross Right Hand Side
+	def cross(self, rhs):
+		N_x = (self.y * rhs.z) - (rhs.y * self.z)
+		N_y = (self.z * rhs.x) - (rhs.z * self.x)
+		N_z = (self.x * rhs.y) - (rhs.x * self.y)
+		N = Vector(Point(N_x, N_y, N_z))
+		return N
+
+	#Normalizes a vector so it has a length of 1
+	def normalize(self):
+		tmp = self.length #Since length is a property it's calculated continuously
+		self.x /= tmp
+		self.y /= tmp
+		self.z /= tmp
 
 
 
-#Line from (3, 3, 3) to (6, 6, 6)
-line_one = Plucker_Coordinates(1, 2, 3, 2 ,2 ,2);
+#Line defined by a line segment DE
+class Line:
+	def __init__(self, D=Point(), E=Point(), t=1):
+		self.slope = Vector(D,E)
+		self.initial_point = D
+		self.t = t
 
-#Line from (-1, -1, -1) to (-3, -3, -3)
-line_two = Plucker_Coordinates(-1, -2, -1, -2, -356, 5);
+	def __str__(self):
+		return "<r> = "+str(self.initial_point)+" + t"+str(self.slope)
 
-print(line_one);
-print(line_two);
+	#This method is used to find a point on the line given the t parameter
+	# def calculate_point(self, t): #FIX ME find a better name for this method
+	# 	M = Point() #The calculatd point
 
-if Side_Operator(line_one, line_two) == 0:
-	print("The lines are parallel: Side-Operator == 0\n");
-else:
-	print("The lines intersect somewhere, can't tell you where though. Sorry bud.\n");
+	# 	M.x = D.x + self.t*(self.slope)
+	# 	M.y = D.y + self.t*(E.y - D.y)
+	# 	M.z = D.z + self.t*(E.z - D.z)
+	# 	return M
+
+
+
+class Plane:
+	#Constructor, initialized to the zero vector if no input given
+	def __init__(self, A=Point(), B=Point(), C=Point()):
+		AB = Vector(A,B)
+		AC = Vector(A,C)
+		N = (AB.cross(AC))
+		N.normalize()
+
+		self.a = N.x
+		self.b = N.y
+		self.c = N.z
+		self.d = -1*((self.a * A.x) + (self.b * A.y) + (self.c *A.z))
+
+	def __str__(self):
+		return "%.2fx + %.2fy + %.2fz + %.2f = 0" %(self.a, self.b, self.c, self.d)
+
+#ABC are the points defining the triangle and the plane
+#DE are the points defining the line and line segment
+# def Plane_intersection_Line(A, B, C, D, E):
+
+
+A = Point(2, 4, 6)
+B = Point(7, 11, 13)
+C = Point(3, 1, 4)
+
+
+D = Point(4, 1, 8)
+E = Point(7, 2, 2)
+print(A,B,C)
+
+AB = Vector(A,B)
+print(AB)
+
+ABC = Plane(A,B,C)
+print(ABC)
+
+DE = Line(D,E)
+print(DE)
+
+
+# print(N.length)
+
+
+# print(N.length)
+
+
+
+
+
+
+
+
 
 
 
