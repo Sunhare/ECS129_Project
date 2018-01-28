0999ac75107a6d0b6fd69e63afb4c1751e174cdb
Sunhare <bgyoume@gmail.com>
2018-01-28 01:51:18 -0800

    Why am I still awake
    Determination is sometimes not a good thing. Geometry3D is close to done, and the actual main loop is a whole load of hot garbage from my sleepy brain

--- Geometry3D.py
+++ Geometry3D.py
@@ -1,38 +1,50 @@
 from math import sqrt
 import numpy as np
 
-class Point: 
+class Point(): 
 	#Constructor, initialized to the zero vector if no input given
 	def __init__(self, x=0, y=0, z=0):
 		self.x = x #x_coordinate
 		self.y = y #y_coordinate
 		self.z = z #z_coordinate
-
 	#Used for when you're printing the object
 	def __str__(self):
-		return ( "(" + str(self.x) + ", " + str(self.y) + ", " + str(self.z) + ")" )
-
+		# return ( "Point: (" + str(self.x) + ", " + str(self.y) + ", " + str(self.z) + ")" )
+		return ( "Point: (%.3f, %.3f, %.3f) " %(self.x, self.y, self.z)) 
 	#Overloading addition and subtraction to handle points
 	def __add__(self, rhs):
-		return ((self.x+rhs.x), (self.y + rhs.y), (self.z + rhs.z))
+		assert type(rhs) is Point, "You can only add points to points"
+		return (self.x+rhs.x), (self.y + rhs.y), (self.z + rhs.z)
 	def __sub__(self, rhs):
-		return ((self.x-rhs.x), (self.y - rhs.y), (self.z - rhs.z))
+		assert type(rhs) is Point, "You can only subtract points from points"
+		return (self.x-rhs.x), (self.y - rhs.y), (self.z - rhs.z)
 
 
 class Vector:
 	#Constructor, initialized to the zero vector if no input given
 	#A = starting point, B = ending point
-	def __init__(self, A = Point(), B = Point()):
+	def __init__(self, A = Point(0,0,0), B = Point(0,0,0)):
+		if type(A) != Point or type(B) != Point:
+			raise TypeError('The vector was not initialized with two points')
 		(self.x, self.y, self.z) = B-A #Does B(x,y,z) – A(x,y,z)
 
-	#The euclidean length is alculated when called
+	#The euclidean length is calculated when called
 	@property
 	def length(self):
 		length = sqrt( (self.x**2 + self.y**2 + self.z**2))
 		return length
 
 	def __str__(self):
-		return ("<" + str(self.x) + ", " +str(self.y)+", "+str(self.z)+">")
+		# return ("Vector: <" + str(self.x) + ", " +str(self.y)+", "+str(self.z)+">")
+		return ( "Vector: <%.3f, %.3f, %.3f> " %(self.x, self.y, self.z)) 
+
+	def __add__(self, rhs):
+		assert type(rhs) is Vector, "You can only add vectors to vectors"
+		return (self.x+rhs.x), (self.y + rhs.y), (self.z + rhs.z)
+
+	def __sub__(self, rhs):
+		assert type(rhs) is Vector, "You can only subtract vectors from vectors"
+		return (self.x-rhs.x), (self.y - rhs.y), (self.z - rhs.z)
 
 	def __mul__(self,scalar):
 		new_position = Point(self.x*scalar, self.y*scalar, self.z*scalar)
@@ -53,23 +65,36 @@ class Vector:
 	#Normalizes a vector so it has a length of 1
 	def normalize(self):
 		tmp = self.length #Since length is a property it's calculated continuously
-		if tmp != 0:
+		if tmp != 0: #Don't divide by zero
 			self.x /= tmp
 			self.y /= tmp
 			self.z /= tmp
 
 
 class Triangle:
-	def __init__(self, A=Point(), B=Point(), C=Point()):
+	def __init__(self, A=Point(0,0,0), B=Point(0,0,0), C=Point(0,0,0)):
 		self.A = A
 		self.B = B
 		self.C = C
 
-	#Check if a point P is inside the triangle
+		self.epsilon = 0.003141592657 #FIXME Chooose an arbitrary epsilon to move flatten the triangle
+		self.threshold = 0.02 #FIXME Choose a threshold to check if the triangle is flat
+		
 	def __str__(self):
-			return ("A: " + str(self.A) + ", B: " + str(self.B) + ", C: " + str(self.C))
+			return ("Triangle:  A: " + str(self.A) + ", B: " + str(self.B) + ", C: " + str(self.C))
 
-	def is_in_triangle(self, P = Point()):
+	def isFlat(self): #FIXME Implement this function
+		#Check if point B is less than self.threshold away
+		#from the line segment AC
+		return False
+
+	def Flatten(self): #FIXME implment this function
+		#Need to move point B closer to the midpoint
+		#of line segment AC by self.epsilon
+		pass
+
+	#Check if a point P is inside the triangle
+	def is_in_triangle(self, P = Point(0,0,0)):
 		A = self.A
 		B = self.B
 		C = self.C
@@ -89,7 +114,7 @@ class Triangle:
 
 class Plane:
 	#Constructor, initialized to the zero vector if no input given
-	def __init__(self, A=Point(), B=Point(), C=Point()):
+	def __init__(self, A=Point(0,0,0), B=Point(0,0,0), C=Point(0,0,0)):
 		self.triangle = Triangle(A,B,C)
 
 		AB = Vector(A,B)
@@ -104,28 +129,29 @@ class Plane:
 
 	#Print the plane equation
 	def __str__(self):
-		return "%.2fx + %.2fy + %.2fz + %.2f = 0" %(self.a, self.b, self.c, self.d)
+		return "Plane: %.2fx + %.2fy + %.2fz + %.2f = 0" %(self.a, self.b, self.c, self.d)
 	
 
 class Line:
-	def __init__(self, A=Point(), B=Point(), t=1):
+	def __init__(self, A=Point(0,0,0), B=Point(0,0,0), t=1):
 		self.slope = Vector(A,B)
 		self.initial_point = A
 		self.t = t
 
 	def __str__(self):
-		return "<r> = "+str(self.initial_point)+" + t"+str(self.slope)
+		return "Line: "+str(self.initial_point)+" + t"+str(self.slope)
 
 	#This property is used to find a point on the line given the t parameter
 	@property
 	def projected_point(self):
-		P = Point() #The calculatd point
-		D = self.initial_point
+		if self.t != None:
+			P = Point() #The calculatd point
+			D = self.initial_point
 
-		P.x = D.x + self.t*(self.slope.x)
-		P.y = D.y + self.t*(self.slope.y)
-		P.z = D.z + self.t*(self.slope.z)
-		return P
+			P.x = D.x + self.t*(self.slope.x)
+			P.y = D.y + self.t*(self.slope.y)
+			P.z = D.z + self.t*(self.slope.z)
+			return P
 
 	def plane_intersection(self, P = Plane()):
 		A = P.a
@@ -135,6 +161,45 @@ class Line:
 		I = self.initial_point
 
 		#Solving for the point where the line intersects the plane
-		self.t = ((-1*D)-(A*I.x)-(B*I.y)-(C*I.z)) / ((A*self.slope.x)+(B*self.slope.y)+(C*self.slope.z))
-
+		try:
+			self.t = ((-1*D)-(A*I.x)-(B*I.y)-(C*I.z)) / ((A*self.slope.x)+(B*self.slope.y)+(C*self.slope.z))
+		except ZeroDivisionError:
+			print("The line and the plane appear to be parallel, tried dividing by zero")
+			# print(str(self)+",\n"+str(P))
+			self.t = None
 		return self.projected_point 
+
+
+#The great function
+def Line_Segment_Intersecting_Triangle(ABC = Plane(), DE = Line()):
+	P = DE.plane_intersection(ABC) #See where the line spanned by DE intersects the plane made by ABC
+	if DE.t != None and DE.t <= 1 and DE.t >= 0: #If the intersection is within the line segment and the lines aren't parallel
+		return ABC.triangle.is_in_triangle(P) #Return whether or not the intersection is within the triangle
+	else: #If the intersection is not within the line segment, return false
+		return False 
+
+
+
+#Only executes if Geometry3D is run directly
+#Geometry3D Test
+if __name__ == "__main__":
+	A = Point(0, 4, 0)
+	B = Point(4, -4, 0)
+	C = Point(-4, -4, 0)
+
+
+	D = Point(4, -4, 0)
+	E = Point(0, 4, 0)
+
+	AB = Vector(A,B)
+	ABC = Plane(A,B,C)
+	DE = Line(D,E)
+
+	print(A) #Point test
+	print(AB) #Vector test
+	print(ABC) #Plane test
+	print(ABC.triangle) #Triangle test
+	print(DE) #Line test
+	print(Line_Segment_Intersecting_Triangle(ABC, DE)) #Line Segment intersecting Triangle test
+
+
--- Project3_ECS129.py
+++ Project3_ECS129.py
@@ -1,55 +1,89 @@
 from Geometry3D import *
-#TO DO:
-#Raise exceptions if incorrect parameters are passed
+
+# file_name = input("Enter the name of the protein file including the handle: ")
+
+file_name = '5pti.txt' # Reads in the file with the amino acid sequence
+
+protein = open(file_name, 'r')
+
+amino_acids = list() #Makes an empty list to store the amino acids of the protein
+
+#Couldn't think of a better way to do this.
+#With this method we need to iterate over the file an additional time
+#Organizes the amino acids into nice coordinates that we can access by index
+for amino_acid in protein:
+	AA = amino_acid
+	AA_coord = list(map( float, AA.split() ))
+	amino_acids.append(AA_coord)
 
 
-#The great function
-def Line_Segment_Intersecting_Triangle(ABC = Plane(), DE = Line()):
-	P = DE.plane_intersection(ABC) #See where the line spanned by DE intersects the plane made by ABC
-	if DE.t <= 1 and DE.t >= 0: #If the intersection is within the line segment
-		return ABC.triangle.is_in_triangle(P) #Return whether or not the intersection is within the triangle
-	else: #If the intersection is not within the line segment, return false
-		return False 
+K_move = 0
+K_like_to_move = 0
+#Main Loop
+
+iteration_period = len(amino_acids)-2
+knot = False
+blocked = False
+counter = 0
+
+while True:
+	i = counter%iteration_period #This is a periodic function going from 0 to a maximum of iteration period -1
+
+	#Make the amino acids into points 3 at a time
+	AA1 = Point(amino_acids[i][0], amino_acids[i][1], amino_acids[i][2])
+	AA2 = Point(amino_acids[i+1][0], amino_acids[i+1][1], amino_acids[i+1][2])
+	AA3 = Point(amino_acids[i+2][0], amino_acids[i+2][1], amino_acids[i+2][2])
+
+	# print(str(AA1) + '\n' + str(AA2) + '\n' + str(AA3) + '\n') #Used for debugging
+
+	#The triangle to check (Every plane has a triangle defining it- Plane.triangle)
+	ABC = Plane(AA1, AA2, AA3)
+
+	#Check the entire protein to ensure there's no line blocking us
+	for j in range(0, len(amino_acids)-1):
+		D = Point(amino_acids[j][0], amino_acids[j][1], amino_acids[j][2])
+		E = Point(amino_acids[j+1][0], amino_acids[j+1][1], amino_acids[j+1][2])
+
+		DE = Line(D,E)
+
+		if Line_Segment_Intersecting_Triangle(ABC, DE) == True: #If nothing is blocking us
+			blocked = True
+			print("There was a block")
+			print(str(ABC.triangle) + str(DE))
+			break
+
+
+	#If you're not blocked, and the triangle isn't flat, flatten the triangle
+	if not blocked and not ABC.triangle.isFlat(): 
+		ABC.triangle.Flatten() #FIXME: Implement this function
+		amino_acids[i+1][0] = ABC.triangle.B.x
+		amino_acids[i+1][1] = ABC.triangle.B.y
+		amino_acids[i+1][2] = ABC.triangle.B.z
+
+		K_move += 1
+
+	#If you're blocked and the triangle is not yet flat, take note
+	elif blocked and not ABC.triangle.isFlat():
+		K_like_to_move += 1
+
+	#After one full iteration, if you haven't made any moves
+	if i == 0 :
+		if K_move == 0 and K_like_to_move == 0:
+			knot = False
+			break
+		elif K_move == 0 and K_like_to_move != 0:
+			knot = True
+			break
+
+		K_move = 0
+		K_like_to_move = 0
+		blocked = False
+
+	counter += 1
+
+print("Is there a knot?: " + str(knot))
 
 
 
-#ABC are the points defining the triangle and the plane
-#DE are the points defining the line and line segment
-# def Plane_intersection_Line(A, B, C, D, E):
 
-
-#Test cases
-A = Point(0, 4, 0)
-B = Point(4, -4, 0)
-C = Point(-4, -4, 0)
-
-
-D = Point(0, 0, 7)
-E = Point(0, 0, -1)
-
-print(A,B,C)
-
-AB = Vector(A,B)
-print(AB)
-
-ABC = Plane(A,B,C)
-print(ABC)
-
-DE = Line(D,E)
-print(DE)
-
-P = DE.plane_intersection(ABC)
-print(P)
-print(DE.t)
-
-print(ABC.triangle)
-print(ABC.triangle.is_in_triangle(P))
-
-
-
-print(Line_Segment_Intersecting_Triangle(ABC, DE))
-
-
-
-
-
+protein.close()
